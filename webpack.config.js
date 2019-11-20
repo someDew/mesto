5936991ff5b2303a27922d9187b0c18bb008f2cd
Andrew Bubnov <jackblack.nt@gmail.com>
2019-11-21 06:20:03 +1100

    Babel installed and setup. Next - install tolls to assembly CSS and HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 
-const path = require('path');
-
+const path = require('path'); // утилита path превращает путь в абсолютный тк WP не понимает относительных
 
 module.exports = {
-    entry: { main: './src/index.js' },
+    entry: { main: './src/index.js' }, // точка входа для wp
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
-    }
+        },
+    module: {
+        rules: [{ // тут описываются правила
+            test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+            use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
+            exclude: /node_modules/ // исключает папку node_modules
+                }
+            ]
+        }
 }
